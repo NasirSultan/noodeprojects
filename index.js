fc92b3a5e9ef8557eb363f7384a9e21bce928729
NasirSultan <rainasirsultan123@gmail.com>
2025-03-02 00:11:06 +0500

    s

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ app.use(express.json());
 app.use(cors());
 
 mongoose
-  .connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .connect(MONGO_URI) // Removed deprecated options
   .then(() => console.log("Connected to MongoDB"))
   .catch((err) => console.error("MongoDB Connection Error:", err));
 
